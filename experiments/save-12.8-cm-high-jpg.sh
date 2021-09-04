ca5497762867b61d4e75f4237880dfbdd89e492c
Todd Kushner <kushnertodd@gmail.com>
2021-09-03 20:36:59 -0600

    added keyboard

--- experiments/save-12.8-cm-high-jpg.sh
+++ experiments/save-12.8-cm-high-jpg.sh
@@ -7,3 +7,5 @@ echo `date`: awake
 echo curl http://192.168.1.243/saved-photo >12.8-cm-high.jpg
 curl http://192.168.1.243/saved-photo >12.8-cm-high.jpg
 echo
+echo cp 12.8-cm-high.jpg 12.8-cm-high.original.jpg
+cp 12.8-cm-high.jpg 12.8-cm-high.original.jpg
